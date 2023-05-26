9afc90d1e8208853682b5f81ea33290fc17c3e09
Jonas Andersson <ja@kth.se>
2023-05-26 10:26:04 +0200

    fix fqdn

--- set-sw-hostnames2.py
+++ set-sw-hostnames2.py
@@ -2,5 +2,5 @@ import requests,json,os
 with open('cnaas-sw-list.json') as f:
    j=json.loads(f.read())
 for (ip,name) in j:
-   x=requests.get("https://sysadm.lan.kth.se/api/v0/set_hostname",params={'ipaddress':ip,'hostname':name},headers={'apilabel':os.environ['KIDDOW_KEY'],'apisecret':os.environ['KIDDOW_SECRET']})
+   x=requests.get("https://sysadm.lan.kth.se/api/v0/set_hostname",params={'ipaddress':ip,'hostname':name+".net.kth.se"},headers={'apilabel':os.environ['KIDDOW_KEY'],'apisecret':os.environ['KIDDOW_SECRET']})
    print(x)
